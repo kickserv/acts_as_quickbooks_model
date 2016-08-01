e51a13d4d2bee26c32aadd81d3eed70fa83df2af
Jonathan Vaught <jonathan.vaught@gmail.com>
2016-08-01 16:25:04 -0400

    Preset configuration for Rails

--- lib/acts_as_quickbooks_model/railtie.rb
+++ lib/acts_as_quickbooks_model/railtie.rb
@@ -5,6 +5,15 @@ module ActsAsQuickbooksModel
   class Railtie < Rails::Railtie
     railtie_name :acts_as_quickbooks_model
 
+    config.before_configuration do
+      ActsAsQuickbooksModel.configure do |config|
+        config.doc_path = Rails.root.join 'doc/acts_as_quickbooks_model'
+        config.definitions_path = Rails.root.join 'doc/acts_as_quickbooks_model/definitions'
+        config.model_maps_path = Rails.root.join 'doc/acts_as_quickbooks_model/model_maps'
+        config.migrations_path = Rails.root.join 'doc/acts_as_quickbooks_model/migrations'
+      end
+    end
+
     rake_tasks do
       load "#{ActsAsQuickbooksModel.root}/lib/tasks/acts_as_quickbooks_model.rake"
     end
